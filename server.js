2c6701bae8767e3784bcde6eabd0c9c21e1f78b6
iamspruce <iamspruce8@gmail.com>
2021-09-01 08:47:16 +0100

    deploy prod ready app

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 //STEP 1: import our app(for everything express)
 const app = require("./app");
 
 //STEP 2: import our dependencies
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 
 // STEP 3: when we have an uncaughtException end the server and log the error
 process.on("uncaughtException", (err) => {
@@ -11,9 +12,6 @@ process.on("uncaughtException", (err) => {
   console.error(err.name, err.message);
   process.exit(1);
 });
-
-// STEP 4: Configure the dotenv to use our config file
-dotenv.config({ path: "./config.env" });
 
 // STEP 4: Get the DATABASE value from the config.env file and store it in a variable DB
 const DB = process.env.DATABASE;
